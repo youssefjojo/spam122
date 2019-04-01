cd47c84d25d242615bd07ef94cf5b50990bfe84b
youssefjojo <46034687+youssefjojo@users.noreply.github.com>
2019-04-02 00:18:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,7 +118,7 @@ client2.on('message', message => {
 
 
 
-if (command == "تحدث") {
+if (command == "say") {
 let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
 if (!rank) return message.reply(' ')
   message.channel.send(args.join("  "))
